01d0b8b8175eec5572a2de5be43e9c10a161f1d8
Bert Tejeda <etejeda@tecknicos.com>
2023-07-13 23:48:26 -0400

    base64-encode output

--- message/container/init.py
+++ message/container/init.py
@@ -1,19 +1,22 @@
+import base64
 import json
 
 def main():
   
   output = {
       "TODAY": [
-      '⚠️ Warning Number 1',
-      '⚠️ Warning Number 2',
+      "⚠️ Warning Number 1",
+      "⚠️ Warning Number 2",
       ],
       "YESTERDAY": [
-      '240+ users have subscribed to Newsletter #1',
-      'The post Post Name was suspended by Nick Mark'
+      "240+ users have subscribed to Newsletter #1",
+      "The post Post Name was suspended by Nick Mark"
       ]
   }
-
-  print(json.dumps(output))
+  output_json = json.dumps(output)
+  output_bytes = output_json.encode("utf-8")
+  output_encoded = base64.b64encode(output_bytes)
+  print(output_encoded.decode("utf-8"))
 
 if __name__ == '__main__':
   main()
